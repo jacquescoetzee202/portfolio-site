8e466f7b3dd76c0cae132abad185e1b5bfd9b64e
Jacques <jacquescoetzee91@gmail.com>
2020-10-30 20:35:02 +0000

    set up gulpfile to run all processes

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,40 @@ let sass = require('gulp-sass');
 let browserSync = require('browser-sync').create();
 let htmlmin = require('gulp-htmlmin');
 let uglify = require('gulp-uglify-es').default;
+let concat = require('gulp-concat');
 
-gulp.task('minify-js' , () => {
-    return gulp.src('js/burger-dev.js')
+gulp.task('concat-js' , () => {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('src/js/'));
+});
+
+gulp.task('uglify-js' , () => {
+    return gulp.src('src/js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest('dist/js/'));
 });
  
 gulp.task('minify-html', () => {
-  return gulp.src('indexDev.html')
+  return gulp.src('src/index.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(rename('index.html'))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('minify-css' , () => {
-    return gulp.src('css/styles.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass', function () {
-	return gulp.src('./scss/main.scss')
+	return gulp.src('src/sass/main.scss')
 		.pipe(sass())
 		.pipe(rename('styles.css'))
-		.pipe(gulp.dest('./css/'));
+		.pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('minify-css' , () => {
+    return gulp.src('src/css/styles.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css/'));
 });
 
 function reload(done) {
@@ -47,13 +54,12 @@ browserSync.init({
 });
 done();
 }
+
+gulp.task('minify-js' ,gulp.series('concat-js','uglify-js'));
+
+gulp.task('minify-sass' , gulp.series('sass' ,'minify-css'));
+
   
 const watching = () => gulp.watch(['./scss/**/*.scss', './js/*.js', 'indexDev.html'], gulp.series('minify-sass', 'minify-html', 'minify-js', reload));
 
 gulp.task('default', gulp.series(serve, watching));
-
-gulp.task('minify-sass' , gulp.series('sass' ,'minify-css'));
-
-gulp.task('sass-watch' , function () {
-    return gulp.watch('scss/**/*.scss', gulp.series('minify-sass'));
-});
